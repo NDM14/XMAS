2405f69d9bad0a2d299e90bd64602846030cf6fc
NDM14 <93001603+NDM14@users.noreply.github.com>
2023-12-24 11:54:45 +0100

    Update helper.py

--- content/helper.py
+++ content/helper.py
@@ -8,6 +8,6 @@ def trenn_die_listen(Liste1, Liste2):
     even = [10, 42, 120, 6, 12, 422]
     
     if Liste1 == even and Liste2 == uneven:
-        return Image(url='https://media.tenor.com/_zUEZJsUsmEAAAAC/that-sure-was-fun-mr-hankey.gif')
-    else:
         return Image(url='Gutschein.png')
+    else:
+        return Image(url='https://media.tenor.com/UnpKBaEgD1sAAAAC/you-know-it-seems-like-something-still-not-right-kyle-broflovski.gif')
